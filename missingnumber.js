da23070d48c28c2d2e1910754d4b67e437d6afb0
veeramalla-anand <anandveeramalla202@gmail.com>
2021-07-16 21:28:20 +0530

    missing number in array

---
+++ missingnumber.js
@@ -0,0 +1,10 @@
+let arr = [1,2,3,4,5,6,7,8,10];
+const findmissingnumber = (arr) => {
+    for (var i = 0;i < arr.length - 1;i++) {
+        if (arr[i] + 1 !=arr[i + 1]){
+            return arr[i]+1;
+        }
+    }
+    return false;   
+}
+console.log(findmissingnumber(arr));
