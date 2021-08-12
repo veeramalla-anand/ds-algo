fb04b910d4e817da1119b5eaf9aedaa835e14e7a
veeramalla-anand <anandveeramalla202@gmail.com>
2021-08-12 22:27:29 +0530

    palindrome

--- palindrome.js
+++ palindrome.js
@@ -1,4 +1,4 @@
-function checkPalindrome(str){
+/*function checkPalindrome(str){
     const len = string.length;
     for(let i= 0;i<len/2;i++){
         if (string[i]!== string[len -1 -i]){
@@ -9,4 +9,14 @@ return 'it is a palindrome';
 }
 const string = "anbcheed";
 const value = checkPalindrome(string);
-console.log(value);
+console.log(value); */
+
+const string = '12321'
+const reverseString = string.split('').reverse().join('');
+if(string == reverseString){
+    console.log(string + " is a palindrome");
+    }
+    else{
+    console.log(string + " is not a palindrome");
+    }
+
